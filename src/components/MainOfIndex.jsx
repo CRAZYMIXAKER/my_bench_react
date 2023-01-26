d1c70cd7e71684e127bb4e0b788a3336c909a2a8
Mikhail Makavetski <zombisuki@gmail.com>
2023-01-26 22:55:25 +0100

    add opportunities section on index page and style it, refactor style for main fonts, install "react-icons"

--- src/components/MainOfIndex.jsx
+++ src/components/MainOfIndex.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import '../styles/components/MainOfIndex.scss';
+// import {RiFilter3Line} from "react-icons/ri";
+import {MdOutlineSearch, MdOutlineFilterList, MdOutlineNotificationAdd, MdOutlineFavoriteBorder} from "react-icons/md";
 
 const MainOfIndex = () => {
     return (
@@ -29,6 +31,31 @@ const MainOfIndex = () => {
             </section>
 
             <section className="opportunities">
+                <h2 className="opportunities-title">
+                    Instantly find new companies and resources relevant to your requests using
+                </h2>
+                <div className="opportunities-list">
+                    <div className="opportunities-list-item">
+                        <MdOutlineSearch/>
+                        <h3>Search</h3>
+                        <p> Our live search allows finding relevant publications by skills in a few seconds </p>
+                    </div>
+                    <div className="opportunities-list-item">
+                        <MdOutlineFilterList/>
+                        <h3>Filters</h3>
+                        <p> Sort specialists by specified filters: Role, Seniority, Stack, Rate, Location.</p>
+                    </div>
+                    <div className="opportunities-list-item">
+                        <MdOutlineNotificationAdd/>
+                        <h3>Notifications</h3>
+                        <p> Adjust notifications to see new publications and get in touch immediately </p>
+                    </div>
+                    <div className="opportunities-list-item">
+                        <MdOutlineFavoriteBorder/>
+                        <h3 className="sc-73f323da-5 hYIBoC">Favorites</h3>
+                        <p> Add publications and companies to your favorite list for the future cooperation </p>
+                    </div>
+                </div>
             </section>
 
             <section className="trial">
