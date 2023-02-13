ae24e1b7864311ceceb0551ea20d004a6b5f1665
Mikhail Makavetski <zombisuki@gmail.com>
2023-02-13 11:08:04 +0100

    try to add new logic for useVacancies
    Took 1 hour 1 minute

--- src/hooks/useVacancies.js
+++ src/hooks/useVacancies.js
@@ -3,35 +3,36 @@ import {useMemo, useState} from "react";
 export const useVacancies = (vacancies, query, filters) => {
     const [result, setResult] = useState(vacancies);
 
-    const filteredAndSearchedVacancies = useMemo(() => {
+    const searchedVacancies = useMemo(() => {
+        const sorted = vacancies.filter(vacancy => vacancy.description.toLowerCase().includes(query.toLowerCase()));
+        setResult(sorted);
+        return sorted;
+    }, [vacancies, query]);
 
-        if (query) {
-            console.log('asda');
-            setResult(result.filter(vacancy => vacancy.description.toLowerCase().includes(query.toLowerCase())));
-        }
+    const filteredVacancies = useMemo(() => {
+        const sorted = [];
 
         if (Object.keys(filters).length > 0) {
-            console.log('jjjjj')
+            console.log('useVacancies - filter')
             // console.log(Object.entries(filters)[0][0]);
             // console.log(Object.entries(filters)[0][1][0].name);
 
-            // result = result.filter(vacancy => vacancy.role.includes('Designer'));
-            // result = Object.entries(filters).map(filter => vacancies.filter[0].includes(filter[1][0].name));
+            const sorted = searchedVacancies.filter(vacancy => vacancy.role.includes('PM'));
+            setResult(sorted);
+
+            // result = Object.entries(filters).map(filter =>
+            // vacancies.filter[0].includes(filter[1][0].name));
 
             // result = Object.entries(filters).map(filter => result.filter(vacancy => {
             //     console.log(filter[1][0].name);
             //     return vacancy[filter[0]].includes(filter[1][0].name);
             // }));
 
-            console.log(filters);
-            console.log(result);
-
             // return Object.entries(filters).map(filter => vacancies.filter[0].includes(filter[1][0].name));
-
         }
 
-        return result;
-    }, [vacancies, query, filters]);
+        return sorted;
+    }, [filters, query]);
 
-    return filteredAndSearchedVacancies;
+    return result;
 }
