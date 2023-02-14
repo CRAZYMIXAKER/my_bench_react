5e4bb6ced4e3d61f8c6ba2a8365e04467593016d
Mikhail Makavetski <zombisuki@gmail.com>
2023-02-14 23:56:16 +0100

    add style for table of list information
    Took 1 hour 7 minutes

--- src/components/InformationList.jsx
+++ src/components/InformationList.jsx
@@ -1,5 +1,7 @@
 import InformationListItem from "./InformationListItem";
 import '../styles/components/InformationList.scss'
+import {MdOutlineArrowDropDown} from "react-icons/md";
+import React from "react";
 
 const InformationList = ({vacancies}) => {
 
@@ -13,7 +15,53 @@ const InformationList = ({vacancies}) => {
 
     return (
         <main className="information-wrapper">
-            {vacancies.map(vacancy => <InformationListItem post={vacancy} key={vacancy.id}/>)}
+            <table role="table" className="table">
+                <tbody className="table-header">
+                <tr role="row">
+                    <td colSpan="1">
+                        <div className="table-td-main"><span></span></div>
+                    </td>
+                    <td colSpan="1">
+                        <div className="table-td-main"><span>Role</span></div>
+                    </td>
+                    <td colSpan="1">
+                        <div className="table-td-main"><span>Seniority</span></div>
+                    </td>
+                    <td colSpan="1">
+                        <div className="table-td-main"><span>Main Stack</span></div>
+                    </td>
+                    <td colSpan="1">
+                        <div className="table-td-main">
+                            <button>
+                                <span>Rate</span>
+                                <span className="svg">
+                                    <MdOutlineArrowDropDown/>
+                                </span>
+                            </button>
+                        </div>
+                    </td>
+                    <td colSpan="1">
+                        <div className="table-td-main"><span>English</span></div>
+                    </td>
+                    <td colSpan="1">
+                        <div className="table-td-main"><span>Location</span></div>
+                    </td>
+                    <td colSpan="1">
+                        <div className="table-td-main">
+                            <button><span>Posted</span>
+                                <span><MdOutlineArrowDropDown/></span>
+                            </button>
+                        </div>
+                    </td>
+                    <td colSpan="1">
+                        <div className="table-td-main"><span></span></div>
+                    </td>
+                </tr>
+                </tbody>
+                <tbody className="table-body" role="rowgroup">
+                {vacancies.map(vacancy => <InformationListItem post={vacancy} key={vacancy.id}/>)}
+                </tbody>
+            </table>
         </main>
     );
 };
--- src/components/InformationListItem.jsx
+++ src/components/InformationListItem.jsx
@@ -1,23 +1,63 @@
 import React from 'react';
+import {MdFavoriteBorder, MdExpandMore} from "react-icons/md";
 
 const InformationListItem = (props) => {
     return (
-        <div style={{display: "flex"}}>
-            {props.post.id}
-            <h1>{props.post.role}</h1>
-            <div>{props.post.seniority}</div>
-            <div>{props.post["main-task"]}</div>
-            <div>{props.post.rate}</div>
-            <div>{props.post.english}</div>
-            <div>{props.post.location}</div>
-            <div>{props.post.posted}</div>
-            <div>{props.post.description}</div>
-            <div>{props.post["company-location"]}</div>
-            <div>{props.post.wordload}</div>
-            <div>@{props.post.telegram}</div>
-            <div>{props.post.email}</div>
-            <div>{props.post.company}</div>
-        </div>
+        <tr role="row">
+            <td role="cell" width="10px">
+                <div></div>
+                <span><MdExpandMore/></span>
+            </td>
+            <td data-title="Role">
+                <div>Role</div>
+                <span>Backend</span></td>
+            <td data-title="Seniority">
+                <div>Seniority</div>
+                <span>Junior</span></td>
+            <td data-title="Main Stack">
+                <div>Main Stack</div>
+                <div>
+                    <div><span>Ruby</span></div>
+                    <div><span>Ruby on Rails</span></div>
+                    <div><span>Javascript</span></div>
+                    <div><span>HTML</span></div>
+                    <div><span>CSS</span></div>
+                </div>
+            </td>
+            <td data-title="Rate">
+                <div>Rate</div>
+                <b>TBD</b></td>
+            <td data-title="English">
+                <div>English</div>
+                <span>Upper-Intermediate</span></td>
+            <td data-title="Location">
+                <div>Location</div>
+                <span>Select location</span></td>
+            <td data-title="Posted">
+                <div>Posted</div>
+                <span>21 days ago</span></td>
+            <td data-title="Favorite">
+                <span>
+                            <MdFavoriteBorder/>
+
+                </span>
+            </td>
+
+            {/*{props.post.id}*/}
+            {/*<h1>{props.post.role}</h1>*/}
+            {/*<div>{props.post.seniority}</div>*/}
+            {/*<div>{props.post["main-task"]}</div>*/}
+            {/*<div>{props.post.rate}</div>*/}
+            {/*<div>{props.post.english}</div>*/}
+            {/*<div>{props.post.location}</div>*/}
+            {/*<div>{props.post.posted}</div>*/}
+            {/*<div>{props.post.description}</div>*/}
+            {/*<div>{props.post["company-location"]}</div>*/}
+            {/*<div>{props.post.wordload}</div>*/}
+            {/*<div>@{props.post.telegram}</div>*/}
+            {/*<div>{props.post.email}</div>*/}
+            {/*<div>{props.post.company}</div>*/}
+        </tr>
     );
 };
 
