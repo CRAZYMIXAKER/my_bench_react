a70149b5cefaa8b8375a9f40cab018b42f11b41a
Mikhail Makavetski <zombisuki@gmail.com>
2023-02-16 19:34:07 +0100

    edit logic for pagination and try to decompose pagination logic to another file
    Took 51 minutes

--- src/components/UI/pagination/Pagination.jsx
+++ src/components/UI/pagination/Pagination.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {getPagesArray} from "../../../utils/pages";
 import "./Pagination.scss"
 import {BsArrowLeftShort, BsArrowRightShort} from "react-icons/bs";
+import MyButton from "../button/MyButton";
 
 const Pagination = ({totalPages, page, changePage, nextPage, prevPage}) => {
     let pagesArray = getPagesArray(totalPages);
@@ -10,34 +11,34 @@ const Pagination = ({totalPages, page, changePage, nextPage, prevPage}) => {
         <div className="pagination">
             <div className="pagination-wrapper">
                 {totalPages > 1 &&
-                    <button
+                    <MyButton
                         className="pagination-wrapper-page _button"
                         onClick={() => prevPage(page)}
                         disabled={page === 1}
                         type="button"
                     >
                         <BsArrowLeftShort/>
-                    </button>
+                    </MyButton>
                 }
 
                 {pagesArray.map(p =>
                     <span
-                        onClick={() => changePage(p)}
                         key={p}
+                        onClick={() => changePage(p)}
                         className={page === p ? 'pagination-wrapper-page _current' : 'pagination-wrapper-page'}>
                         {p}
                     </span>
                 )}
 
                 {totalPages > 1 &&
-                    <button
+                    <MyButton
                         className="pagination-wrapper-page _button"
                         onClick={() => nextPage(page)}
                         disabled={page === totalPages}
                         type="button"
                     >
                         <BsArrowRightShort/>
-                    </button>
+                    </MyButton>
                 }
             </div>
         </div>
--- src/utils/pages.js
+++ src/utils/pages.js
@@ -1,11 +1,38 @@
+export const scrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+}
+
 export const getPageCount = (totalCount, limit) => {
     return Math.ceil(totalCount / limit)
 }
 
 export const getPagesArray = (totalPages) => {
     let result = [];
-    for(let i = 0; i < totalPages; i++){
+    for (let i = 0; i < totalPages; i++) {
         result.push(i + 1)
     }
     return result;
+}
+
+export const changePage = (page) => {
+    scrollToTop();
+    return page;
+}
+
+export const prevPage = (page) => {
+    if (page !== 1) {
+        scrollToTop();
+        return page - 1;
+    }
+}
+
+export const nextPage = (page, totalPages) => {
+    if (page < totalPages) {
+        scrollToTop();
+        return page + 1;
+    }
 }
